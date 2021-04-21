016bc28bb9ff512e66e784d7ab649db8e83f4425
breney <brunopy2@gmail.com>
2021-04-21 21:20:26 +0100

    css

--- server.js
+++ server.js
@@ -4,11 +4,14 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 var game = require('./src/game');
 
+
 app.get('/', function(req,res){
-    res.send(__dirname + '/views/game.html');
+    res.sendFile(__dirname + '/src/views/game.html');
     console.log(__dirname);
 })
 
 io.on('connection', function(socket){
 
 });
+
+app.listen(3000, () => {   console.log('Example app listening at http://localhost:3000')});
