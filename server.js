722bb152db8b6a9eeb2832217e7a8bfd9ef76ae1
breney <brunopy2@gmail.com>
2021-04-22 16:50:38 +0100

    socket html

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
-console.log('ola');
-var app = require('express')();
+const nm_dependencies = ['socket.io']; 
+const express = require('express');
+var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
-var game = require('./src/game');
+var game = require('./src/game').createGame();
+var path = require('path');
 
+nm_dependencies.forEach(dep => {   app.use(`/${dep}`, express.static(path.resolve(`node_modules/${dep}`)))});
+
+console.log(game.width);
 
 app.get('/', function(req,res){
     res.sendFile(__dirname + '/src/views/game.html');
-    console.log(__dirname);
+
 })
 
 io.on('connection', function(socket){
-    console.log(game);
+    
 });
+
+io.on('test' , function(){
+    console.log('test');
+})
 
 app.listen(3000, () => {   console.log('Example app listening at http://localhost:3000')});
--- src/game.js
+++ src/game.js
@@ -1,26 +1,27 @@
-function createGame(){
-    var game = {
-        width: 35,
-        height: 30,
-        players: {
-            x : 0,
-            y : 0,
-            score : 0
-        },
-        fruits: {},
-        addPlayer,
-        removePlayer
+module.exports = {
+     createGame: function() {
+        var game = {
+            width: 500,
+            height: 500,
+            players: {},
+            fruits: {},
+            addPlayer,
+            removePlayer,
+          
+        }
+    
+        function addPlayer(socketId){
+            return game.players[socketId] = {
+                x : (Math.random() * game.width),
+                y : (Math.random() * game.height)
+            };
+        }
+    
+        function removePlayer(socketId){
+            delete game.players[socketId];
+        }
+
+        return game;
     }
-}
-
-function addPlayer(socketId){
-    return game.players[socketId] = {
-        x : (Math.random() * game.width),
-        y : (Math.random() * game.height)
-    };
-}
-
-function removePlayer(socketId){
-    delete game.players[socketId];
 }
 
